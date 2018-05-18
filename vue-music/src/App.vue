0e3dc9bf3476ac3112ead2f61279cefd8825f513
quadrant26 <quadrant26@163.com>
2018-05-18 17:18:14 +0800

    add m-header and tab compontens

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <!--<router-view/>-->
-    hello world
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    components: {
+      MHeader,
+      Tab
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
