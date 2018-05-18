83ad7791ff117d5a480fea107d23bd9a0ec424ea
quadrant26 <quadrant26@163.com>
2018-05-18 17:20:33 +0800

    add resolve/alias

--- vue-music/build/webpack.base.conf.js
+++ vue-music/build/webpack.base.conf.js
@@ -35,7 +35,10 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'src': resolve('src'),
-      'common': resolve('src/common')
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'base': resolve('src/base'),
+      'api': resolve('src/api')
     }
   },
   module: {
