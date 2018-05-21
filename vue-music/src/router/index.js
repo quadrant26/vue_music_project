1662037fd73d72a5d684cb540de1b1077379f413
quadrant26 <quadrant26@163.com>
2018-05-21 13:32:30 +0800

    change router

--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Recommend from 'components/recommend/recommend'
-import Singer from 'components/search/search'
+import Singer from 'components/singer/singer'
 import Rank from 'components/rank/rank'
 import Search from 'components/search/search'
 
@@ -12,22 +12,22 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/recommend'
+      redirect: 'recommend'
     },
     {
-      path: '/recommend',
+      path: 'recommend',
       componetn: Recommend
     },
     {
-      path: '/singer',
+      path: 'singer',
       componetn: Singer
     },
     {
-      path: '/rank',
+      path: 'rank',
       componetn: Rank
     },
     {
-      path: '/search',
+      path: 'search',
       componetn: Search
     }
   ]
