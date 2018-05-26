ad9c372543e40473b57962fa175b0326a0ad22ee
quadrant26 <quadrant26@163.com>
2018-05-26 12:19:50 +0800

    require axios and express, get url by qq

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -20,7 +20,7 @@
 
 <script>
 import Slider from 'base/slider/slider'
-import {getRecommend} from "api/recommend"
+import {getRecommend, getDiscList } from "api/recommend"
 import {ERR_OK} from "api/config"
 
 export default {
@@ -30,14 +30,23 @@ export default {
     }
   },
   created (){
-    this._getRecommend();
+    this._getRecommend()
+
+    this._getDiscList()
   },
   methods: {
     _getRecommend (){
       getRecommend().then( (res) => {
         if (res.code === ERR_OK ){
-          console.log(res.data.slider)
+          // console.log(res.data.slider)
           this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList (){
+      getDiscList().then((res) => {
+        if ( res.code === ERR_OK) {
+          console.log(res);
         }
       })
     }
