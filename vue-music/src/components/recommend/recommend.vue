eb1f9fd01b507307610c9a3dff5ee6810b48f71d
quadrant26 <quadrant26@163.com>
2018-05-26 15:16:19 +0800

    add components loading

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -7,7 +7,7 @@
           <slider>
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl" alt="">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl" alt="">
               </a>
             </div>
           </slider>
@@ -17,7 +17,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl" alt="">
+                <img width="60" height="60" v-lazy="item.imgurl" alt="">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -28,12 +28,16 @@
         </div>
         <!-- -->
       </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
 import Slider from 'base/slider/slider'
 import {getRecommend, getDiscList } from "api/recommend"
 import {ERR_OK} from "api/config"
@@ -78,7 +82,8 @@ export default {
   },
   components: {
     Slider,
-    Scroll
+    Scroll,
+    Loading
   }
 }
 </script>
--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -3,12 +3,16 @@ import App from './App'
 import router from './router'
 
 import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
 FastClick.attach(document.body)
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
