d4fce0785cb045fbe04dd7a26ae2ddaae7ab2d85
quadrant26 <quadrant26@163.com>
2018-05-26 14:57:05 +0800

    scroll and refresh song list

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length" class="slider-wrapper">
-        <slider>
-          <div v-for="item in recommends">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl" alt="">
-            </a>
-          </div>
-        </slider>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li v-for="item in discList" class="item">
-            <div class="icon">
-              <img width="60" height="60" :src="item.imgurl" alt="">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <!-- -->
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl" alt="">
+              </a>
             </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- -->
       </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+import Scroll from 'base/scroll/scroll'
 import Slider from 'base/slider/slider'
 import {getRecommend, getDiscList } from "api/recommend"
 import {ERR_OK} from "api/config"
@@ -41,8 +46,9 @@ export default {
     }
   },
   created (){
+    // banner
     this._getRecommend()
-
+    // 歌单
     this._getDiscList()
   },
   methods: {
@@ -57,14 +63,22 @@ export default {
     _getDiscList (){
       getDiscList().then((res) => {
         if ( res.code === ERR_OK) {
-          console.log(res);
+          // console.log(res);
           this.discList = res.data.list
         }
       })
+    },
+    loadImage (){
+      if (!this.checkLoaded){
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
+
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   }
 }
 </script>
