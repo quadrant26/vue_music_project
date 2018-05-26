49bff76abb2fa62126b01a70c27007cf7f036a09
quadrant26 <quadrant26@163.com>
2018-05-26 14:32:48 +0800

    添加歌单组件信息

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -12,7 +12,17 @@
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
-        <ul></ul>
+        <ul>
+          <li v-for="item in discList" class="item">
+            <div class="icon">
+              <img width="60" height="60" :src="item.imgurl" alt="">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -26,7 +36,8 @@ import {ERR_OK} from "api/config"
 export default {
   data (){
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created (){
@@ -47,6 +58,7 @@ export default {
       getDiscList().then((res) => {
         if ( res.code === ERR_OK) {
           console.log(res);
+          this.discList = res.data.list
         }
       })
     }
