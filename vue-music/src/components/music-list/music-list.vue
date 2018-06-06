111f80ab93d5ebed09e414982ed755414fc3af31
quadrant26 <quadrant26@163.com>
2018-06-06 18:14:00 +0800

    @select="selectItem" -> selectItem(item, index)

--- vue-music/src/components/music-list/music-list.vue
+++ vue-music/src/components/music-list/music-list.vue
@@ -14,7 +14,7 @@
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
       <div class="song-list-wrapper">
-        <song-list :songs="songs"></song-list>
+        <song-list @select="selectItem" :songs="songs"></song-list>
       </div>
       <div class="loading-container" v-show="!songs.length">
         <loading></loading>
@@ -27,7 +27,8 @@
   import Scroll from 'base/scroll/scroll'
   import SongList from 'base/song-list/song-list'
   import Loading from 'base/loading/loading'
-  import { prefixStyle} from "common/js/dom";
+  import { prefixStyle} from "common/js/dom"
+  import { mapActions } from 'vuex'
 
   const RESERVED_HEIGHT = 40
   const transform = prefixStyle('transform')
@@ -73,7 +74,16 @@
       },
       back (){
         return this.$router.back()
-      }
+      },
+      selectItem (item, index){
+        this.selectPlay({
+          list: this.songs,
+          index: index
+        })
+      },
+      ...mapActions([
+        'selectPlay'
+      ])
     },
     watch: {
       scrollY (newY){
