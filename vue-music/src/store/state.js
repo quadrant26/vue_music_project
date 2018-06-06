88f75bd49629f687ae499e350509f1d9961a691f
quadrant26 <quadrant26@163.com>
2018-06-06 16:42:02 +0800

    Vuex 播放器 state 控制

--- vue-music/src/store/state.js
+++ vue-music/src/store/state.js
@@ -1,5 +1,13 @@
+import { playMode } from "common/js/config";
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,   // 播放状态
+  fullScreen: false, // 全屏设置
+  playList: [],
+  sequenceList: [],
+  mode: playMode.sequence, // 设置播放模式
+  currentIndex: -1 // 当前
 }
 
 export default state
