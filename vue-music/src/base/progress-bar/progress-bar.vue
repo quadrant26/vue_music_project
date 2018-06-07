5d506dc538b9ada124bbc516cb0249424a93a677
quadrant26 <quadrant26@163.com>
2018-06-07 16:17:05 +0800

    修复点击进度条精度不准

--- vue-music/src/base/progress-bar/progress-bar.vue
+++ vue-music/src/base/progress-bar/progress-bar.vue
@@ -50,7 +50,11 @@
 
       },
       progressClick (e){
-        this._offset(e.offsetX)
+        const rect = this.$refs.progressBar.getBoundingClientRect()
+        const offsetWidth = e.pageX - rect.left
+        this._offset(offsetWidth)
+        // 当我们点击 progressBtn 的时候，e.offsetX 获取不对
+        // this._offset(e.offsetX)
         this._triggerPercent()
       },
       _triggerPercent (){
