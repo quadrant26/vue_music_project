46c6788a8f6b19d6115ae7b95ac832eb40589a41
quadrant26 <quadrant26@163.com>
2018-06-08 16:18:37 +0800

    scrollToElement

--- vue-music/src/base/scroll/scroll.vue
+++ vue-music/src/base/scroll/scroll.vue
@@ -59,8 +59,8 @@
       scrollTo (){
         this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
       },
-      scrollTopElement (){
-        this.scroll && this.scroll.scrollTopElement.apply(this.scroll, arguments)
+      scrollToElement (){
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
       }
     },
     watch: {
