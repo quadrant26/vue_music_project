a85e2d8a5dae2190d70ed636faa29a2331947461
quadrant26 <quadrant26@163.com>
2018-06-08 16:56:44 +0800

    touchstart and move show 'cd' or 'lyric'

--- vue-music/src/components/player/player.vue
+++ vue-music/src/components/player/player.vue
@@ -15,8 +15,8 @@
             <h1 class="title" v-html="currentSong.name"></h1>
             <h2 class="subtitle" v-html="currentSong.singer"></h2>
           </div>
-          <div class="middle">
-            <div class="middle-l">
+          <div class="middle" @touchstart.prevent="middleTouchStart" @touchmove.prevent="middleTouchMove" @touchend="middleTouchEnd">
+            <div class="middle-l" ref="middleL">
               <div class="cd-wrapper" ref="cdWrapper">
                 <div class="cd" :class="cdCls">
                   <img :src="currentSong.image" alt="" class="image">
@@ -32,6 +32,10 @@
             </Scroll>
           </div>
           <div class="bottom">
+            <div class="dot-wrapper">
+              <span class="dot" :class="{'active': currentShow === 'cd'}"></span>
+              <span class="dot" :class="{'active': currentShow === 'lyric'}"></span>
+            </div>
             <div class="progress-wrapper">
               <span class="time time-l">{{format(currentTime)}}</span>
               <div class="progress-bar-wrapper">
@@ -85,6 +89,7 @@
   import Scroll from 'base/scroll/scroll'
 
   const transform = prefixStyle('transform')
+  const transitionDuration = prefixStyle('transitionDuration')
 
   export default {
     data (){
@@ -125,6 +130,9 @@
         'mode', // 播放模式
         'sequenceList'
       ])
+    },
+    created (){
+      this.touch = {}
     },
     methods: {
       back (){
@@ -277,8 +285,62 @@
           let lineEl = this.$refs.lyricLine[lineNum-5]
           this.$refs.lyricLine.scrollToElement(lineEl, 1000)
         } else{
-          this.$refs.lyricLine.scrollTo(0, 0, 1000)
+          this.$refs.lyricList.scrollTo(0, 0, 1000)
         }
+      },
+      middleTouchStart (e) {
+        this.touch.initiated = true
+        const touch = e.touches[0]
+        this.touch.startX = touch.pageX
+        this.touch.startY = touch.pageY
+      },
+      middleTouchMove (e) {
+        if ( !this.touch.initiated )return
+
+        const touch = e.touches[0]
+        const deltaX = touch.pageX - this.touch.startX
+        const deltaY = touch.pageY - this.touch.startY
+
+        if ( Math.abs(deltaY) > Math.abs(deltaX) ) {
+          return
+        }
+
+        const left = this.currentShow === 'cd' ? 0 : -window.innerWidth
+        const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
+        this.touch.percent = Math.abs(offsetWidth / window.innerWidth)
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = 0
+        this.$refs.middleL.style.opacity = 1 - this.touch.percent
+        this.$refs.middleL.style[transitionDuration] = 0
+      },
+      middleTouchEnd (e) {
+        let offsetWidth
+        let opacity
+
+        if (this.currentShow === 'cd') {
+          if (this.touch.percent > 0.1){
+            offsetWidth = -window.innerWidth
+            opacity = 0
+            this.currentShow = 'lyric'
+          } else {
+            offsetWidth = 0
+            opacity = 1
+          }
+        } else {
+          if ( this.touch.percent < 0.9 ){
+            offsetWidth = 0
+            this.currentShow = 'cd'
+            opacity = 1
+          } else {
+            offsetWidth = -window.innerWidth
+            opacity = 0
+          }
+        }
+        const time = 30
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = time
+        this.$refs.middleL.style.opacity = opacity
+        this.$refs.middleL.style[transitionDuration] = 0
       },
       _pad (num, n=2){
         let len = num.toString().length
