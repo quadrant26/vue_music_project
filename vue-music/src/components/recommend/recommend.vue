93b41aebee35f666d60e26f3d11857f8be7ecab0
quadrant26 <quadrant26@163.com>
2018-06-11 15:15:15 +0800

    点击歌单跳转到歌单详情页

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -15,7 +15,7 @@
         <div class="recommend-list">
           <h1 class="list-title">热门歌单推荐</h1>
           <ul>
-            <li v-for="item in discList" class="item">
+            <li v-for="item in discList" class="item" @click="selectItem(item)">
               <div class="icon">
                 <img width="60" height="60" v-lazy="item.imgurl" alt="">
               </div>
@@ -32,6 +32,7 @@
         <loading></loading>
       </div>
     </scroll>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -42,6 +43,7 @@ import Slider from 'base/slider/slider'
 import {getRecommend, getDiscList } from "api/recommend"
 import {ERR_OK} from "api/config"
 import { playlistMixin} from 'common/js/mixin'
+import { mapMutations } from 'vuex'
 
 export default {
   mixins: [playlistMixin],
@@ -62,6 +64,12 @@ export default {
       const bottom = playlist.length> 0 ? '60px' : ''
       this.$refs.recommend.style.bottom = bottom
       this.$refs.scroll.refresh()
+    },
+    selectItem (item){
+      this.$router.push({
+        path: `/recommend/${item.dissid}`
+      })
+      this.setDisc(item)
     },
     _getRecommend (){
       getRecommend().then( (res) => {
@@ -85,7 +93,10 @@ export default {
         this.checkLoaded = true
       }
 
-    }
+    },
+    ...mapMutations({
+      setDisc: 'SET_DISC'
+    })
   },
   components: {
     Slider,
