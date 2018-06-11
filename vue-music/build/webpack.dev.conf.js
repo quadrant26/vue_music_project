e0c09f8515045990a3483132cb0139aa053a75eb
quadrant26 <quadrant26@163.com>
2018-06-11 15:13:01 +0800

    api/dissList 获取歌单信息

--- vue-music/build/webpack.dev.conf.js
+++ vue-music/build/webpack.dev.conf.js
@@ -88,6 +88,21 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         }).catch( (e) => {
           console.log(e)
         })
+      }),
+      app.get('/api/dissList', function (req, res){
+        var url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+
+        axios.get(url, {
+          headers: {
+            referer: `https://y.qq.com/n/yqq/playlist/${req.query.disstid}.html`,
+            host: 'c.y.qq.com'
+          },
+          params: req.query
+        }).then( (response) => {
+          res.json(response.data)
+        }).catch( (e) => {
+          console.log(e)
+        })
       })
     }
   },
