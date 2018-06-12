ae0f7737c136f17f6e5435715e746ea34956acb0
quadrant26 <quadrant26@163.com>
2018-06-12 16:41:51 +0800

    add jsonp method getMusicList()

--- vue-music/src/api/rank.js
+++ vue-music/src/api/rank.js
@@ -13,3 +13,18 @@ export function getTopList (){
 
   return jsonp(url, data, options)
 }
+
+export function getMusicList(topid){
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    needNewCode: 1,
+    tpl: 3,
+    page: 'detail',
+    type: 'top',
+    topid: topid
+  })
+
+  return jsonp(url, data, options)
+}
