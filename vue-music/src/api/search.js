35a6be74226f1c5bd7af2425884284ab53632e01
quadrant26 <quadrant26@163.com>
2018-06-13 16:47:31 +0800

    add function search method

--- vue-music/src/api/search.js
+++ vue-music/src/api/search.js
@@ -13,3 +13,27 @@ export function getHotKey(){
 
   return jsonp(url, data, options)
 }
+
+export function search (query, page, zhida) {
+  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    needNewCode: 1,
+    w: query,
+    zhidaqu: 1,
+    catZhida: zhida ? 1 : 0,
+    t: 0,
+    flag: 1,
+    ie: 'utf-8',
+    sem: 1,
+    aggr: 0,
+    perpage: 20,
+    n: 20,
+    p: page,
+    remoteplace: 'txt.mqq.all',
+    uin: 0
+  })
+
+  return jsonp(url, data,  options)
+}
