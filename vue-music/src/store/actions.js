82513c0f925895d8f453872998bc2478ed4c4163
quadrant26 <quadrant26@163.com>
2018-06-25 15:20:27 +0800

    搜索结果歌曲插入歌单 mapActions

--- vue-music/src/store/actions.js
+++ vue-music/src/store/actions.js
@@ -33,3 +33,47 @@ export const randomPlay = function ({commit}, {list}) {
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
 }
+
+export const insertSong = function ( {commit, state}, song){
+  let playlist = state.playList.slice()
+  let sequencelist = state.sequenceList.slice()
+  let currentIndex = state.currentIndex
+
+  // 记录当前歌曲
+  let curentSong = playlist[currentIndex]
+  // 查找当前列表时候有待插入的歌曲并返回其索引
+  let fpIndex = findIndex(playlist, song)
+  // 插入歌曲， 索引 +1
+  currentIndex ++
+  // 插入这首歌到当前索引位置
+  playlist.splice(currentIndex, 0, song)
+  // 如果已经包含这首歌
+  if ( fpIndex > -1){
+    // 当前插入的序号大于列表中的序号
+    if ( currentIndex > fpIndex ){
+      playlist.splice(fpIndex, 1)
+      currentIndex --
+    } else{
+      playlist.splice(fpIndex+1, 1)
+    }
+  }
+
+  let currentSIndex = findIndex(sequencelist, curentSong) + 1
+
+  let fsIndex = findIndex(sequencelist, song)
+  sequencelist.splice(currentIndex,  0, song)
+
+  if ( fsIndex > -1){
+    if ( currentIndex > fsIndex){
+      sequencelist.splice(fsIndex, 1)
+    } else {
+      sequencelist.splice(fsIndex+1, 1)
+    }
+  }
+
+  commit(types.SET_PLAY_LIST, playlist)
+  commit(types.SET_SEQUENCE_LIST, sequencelist)
+  commit(types.SET_CURRENT_INDEX, currentIndex)
+  commit(types.SET_FULL_SCREEN, true)
+  commit(types.SET_PLAYING_STATE, true)
+}
