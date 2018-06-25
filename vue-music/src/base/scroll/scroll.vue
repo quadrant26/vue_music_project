7beaaa5fca022f2db10f610dc7592b69dd20ec0a
quadrant26 <quadrant26@163.com>
2018-06-25 14:39:20 +0800

    添加下拉刷新 pullup -> scrollToEnd

--- vue-music/src/base/scroll/scroll.vue
+++ vue-music/src/base/scroll/scroll.vue
@@ -23,6 +23,10 @@
       listenScroll: {
         type: Boolean,
         default: false
+      },
+      pullup: {
+        type: Boolean,
+        default: false
       }
     },
     mounted (){
@@ -44,6 +48,14 @@
           var me = this;
           this.scroll.on('scroll', (pos)=>{
             me.$emit('scroll', pos)
+          })
+        }
+
+        if ( this.pullup ){
+          this.scroll.on('scrollEnd', ()=>{
+            if (this.scroll.y <= (this.scroll.maxScrollY + 50)){
+              this.$emit('scrollToEnd')
+            }
           })
         }
       },
